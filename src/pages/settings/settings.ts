d3af5dc4f784de8fd2b4167d177ba6379ba07ac2
iliyaml <mohamad.i.mohamad.lokman@vanderbilt.edu>
2017-10-15 11:13:06 -0500

    Implemented settings page

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the SettingsPage page.
@@ -14,12 +16,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'settings.html',
 })
 export class SettingsPage {
+  location: {
+    city: string,
+    state: string
+  }
+  city: string;
+  state: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private storage: Storage) {
+      this.storage.get('location').then(val => {
+        this.location = (val != null) ? JSON.parse(val) : { city: 'Nashville', state: 'TN' };
+        this.city = this.location.city;
+        this.state = this.location.state;
+      });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
+  }
+
+  saveForm(){
+    let location = {
+      city: this.city,
+      state: this.state
+    };
+
+    this.storage.set('location', JSON.stringify(location));
+
+    // Navigate to HomePage
+    this.navCtrl.push(HomePage);
   }
 
 }
